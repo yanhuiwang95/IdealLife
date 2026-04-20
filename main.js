90b4b68a03ca574d36270e5350f31fb3a6520e5d
yanhuiwang95 <151680750+yanhuiwang95@users.noreply.github.com>
2026-04-20 17:23:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -520,8 +520,7 @@ document.addEventListener("DOMContentLoaded", function () {
           <ul>
             <li><a href="/should.html">应该做</a></li>
             <li><a href="/like.html">喜欢做</a></li>
-            <li><a href="/meaning.html">人生意义</a></li>
-            <li><a href="/memories.html">时光留影</a></li>
+            <li><a href="/life-purpose.html">人生意义</a></li>
           </ul>
         </nav>
 
